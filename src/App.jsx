94eb9d132f4cdd536fdbc8994d66f8ccef5e9b80
jaqwe2301 <jaqwe2301@naver.com>
2023-09-30 01:01:00 +0900

    Feat: Route 설치, 비밀번호 유효성 검사

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
+import Main from "./pages/Main";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
