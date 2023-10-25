8552eb9d0c536bc3f1bd0ae7ec9225fcbf37564b
jaqwe2301 <jaqwe2301@naver.com>
2023-10-25 23:03:04 +0900

    Feat: Main 화면 퍼블리싱

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,76 +1,77 @@
 import React from "react";
-// import { useEffect, useState } from "react";
-// import { GlobalColors } from "../styles/Styles";
-import "../css/splashTitle.css";
-
-import backgroundImage from "../assets/splash/splash-background.png";
-import { ReactComponent as SplashIconX } from "../assets/splash/splash-icon1.svg";
-import { ReactComponent as SplashIconPlus } from "../assets/splash/splash-icon2.svg";
+import { useState, useEffect } from "react";
+import SplashScreen from "../components/SplashScreen";
+import mainBackground from "../assets/main-background.png";
 
 function Main() {
+  const [splash, setSplash] = useState(true);
+
+  // useEffect(() => {
+  //   const lastSplashTime = localStorage.getItem("lastSplashTime");
+
+  //   // 현재 시간과 마지막 스플래시 화면 표시 시간을 비교합니다.
+  //   const currentTime = new Date();
+  //   if (lastSplashTime) {
+  //     const nextSplashTime = new Date(JSON.parse(lastSplashTime));
+  //     nextSplashTime.setDate(nextSplashTime.getDate() + 1); // 마지막으로 스플래시 화면이 표시된 후 하루가 지났는지 확인합니다.
+
+  //     if (currentTime >= nextSplashTime) {
+  //       setSplash(true); // 하루가 지났으면 스플래시 화면을 다시 표시합니다.
+  //     }
+  //   } else {
+  //     setSplash(true); // 스플래시 화면을 처음 표시하는 경우입니다.
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    // if (splash) {
+    // 현재 시간을 로컬 스토리지에 저장하여 다음에 스플래시 화면을 표시할 시간을 계산하는 데 사용합니다.
+    // const currentTime = new Date();
+    // localStorage.setItem("lastSplashTime", JSON.stringify(currentTime));
+
+    // 일정 시간이 지난 후에 스플래시 화면을 숨기는 로직
+    const timeout = setTimeout(() => {
+      setSplash(false);
+    }, 5500);
+
+    return () => clearTimeout(timeout);
+    // }
+    // }, [splash]);
+  }, []);
+
   const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
+    backgroundImage: `url(${mainBackground})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
     height: "100vh",
+    backgroundColor: "grey",
   };
 
   return (
     <div className="h-full">
-      <div style={backgroundStyle} className="pt-[6%]">
-        <div className="flex justify-end items-center mr-[-28px]">
-          <SplashIconX
-            height="66"
-            className="mr-[-14px] animate-fadeInOutSlow"
-          />
-          <SplashIconPlus
-            height="32"
-            className="mr-[-14px] animate-fadeInOutNormal"
-          />
-          <SplashIconX height="66" className="animate-fadeInOutFast" />
+      {splash ? (
+        <SplashScreen />
+      ) : (
+        <div
+          style={backgroundStyle}
+          className="flex flex-col items-center justify-around px-[20px] pb-8"
+        >
+          <div />
+          <div className="flex flex-col items-center gap-3 mb-8">
+            <p className="text-white text-[32px] font-bold">SNUGGLE</p>
+            <p className="text-white text-[18px]">너를 위한 스테이지그라운드</p>
+          </div>
+          <div className="flex flex-col items-center gap-4 w-full">
+            <button className="w-full max-w-[420px] h-[58.76px] text-white bg-signature font-semibold rounded-lg">
+              로그인
+            </button>
+            <button className="w-full max-w-[420px] h-[58.76px] bg-white text-signature font-semibold rounded-lg">
+              회원가입
+            </button>
+          </div>
         </div>
-        {/* <p className="w-full text-center text-white font-bold text-[32px] mt-[60px]">
-          SNUGGLE
-        </p> */}
-        <div className="flex justify-center w-full mt-[44px]">
-          <svg className="intro go" viewBox="0 0 200 86">
-            <text
-              text-anchor="middle"
-              x="100"
-              y="30"
-              className="text text-stroke"
-              clip-path="url(#text1)"
-            >
-              SNUGGLE
-            </text>
-            <text
-              text-anchor="middle"
-              x="100"
-              y="30"
-              className="text text-stroke text-stroke-2"
-              clip-path="url(#text1)"
-            >
-              SNUGGLE
-            </text>
-            <defs>
-              <clipPath id="text1">
-                <text text-anchor="middle" x="100" y="30" className="text">
-                  SNUGGLE
-                </text>
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
-        <div className="absolute bottom-[80px] flex items-center w-full gap-[14px]">
-          <SplashIconX height="66" className="animate-fadeInOutSlow" />
-          <SplashIconX height="66" className="animate-fadeInOutNormal" />
-        </div>
-      </div>
-      {/* <div
-        className="absolute bottom-0 h-[67px] w-full max-w-[480px]"
-        style={{ backgroundColor: GlobalColors.white }}
-      ></div> */}
+      )}
     </div>
   );
 }
