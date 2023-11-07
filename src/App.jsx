81452788bde72c95bfe9675a57642c177bede4fc
jessy <yaeunjess@gmail.com>
2023-11-08 01:52:40 +0900

    :lipstick: [COMPLETE] PerformDetailScreen UI
    by jessy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./index.css";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import Map from "./pages/Map";
+import Home from "./pages/Home";
+import PerformDetail from "./pages/PerformDetail";
+import Navigation from "./pages/Navigation";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="map" element={<Map />}/>
+        <Route path="home" element={<Home />}/>
+        <Route path="perform/detail" element={<PerformDetail />}/>
+        <Route path="navigation" element={<Navigation />}/>
       </Routes>
     </BrowserRouter>
   );
