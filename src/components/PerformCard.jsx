f0ff4838fd034c7b8bbce283e37b3ee56bf499e8
jessy <yaeunjess@gmail.com>
2023-11-09 12:40:05 +0900

    :lipstick: [ADD] Component PerformCard.jsx
    by jessy

---
+++ src/components/PerformCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import scrapIcon from '../assets/all-icon-scrap.svg'
+import locationIcon from '../assets/all-icon-location-gray.svg'
+
+
+export default function PerformCard({img, tagFontSize, titleFontSize ,locationFontSize,
+    date, payment, title, location, scraped}) {
+  return (
+    <div className={`flex flex-col rounded bg-zinc-200 w-full h-full`}>
+      
+        <img className={`w-full h-1/2 rounded-t`} src={img}/>
+        {/* <div>콘서트</div> */}
+      <div className={`w-full h-1/2 flex flex-row w-full `}>
+        <div className='flex flex-col gap-1 h-full w-full content-center my-2 ml-2'>
+            <div className={`flex flex-row gap-1 w-full`}>
+                <div className={`w-fit h-fit px-[4px] bg-zinc-300
+                font-bold text-[${tagFontSize}] text-zinc-500`}>{date}</div>
+                <div className={`w-fit h-fit px-[4px] bg-zinc-300
+                font-bold text-[${tagFontSize}] text-zinc-500`}>{payment}</div>
+            </div>
+            <p className={`text-[${titleFontSize}] font-semibold`}>{title}</p>
+            <div className='flex flex-row w-fit h-fit gap-1'>
+                <img src={locationIcon}/>
+                <div className={`font-normal text-zinc-500 text-[${locationFontSize}]`}>{location}</div>
+            </div>
+        </div>
+        <img src={scrapIcon} className='w-1/6 h-1/5 mt-2'/>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PerformCard from '../components/PerformCard'
+import PerformThumbnail from '../assets/performDetail/performDetail-thumbnail.jpg'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='w-full h-full'>
+      <div className='w-[164px] h-[136px]'>
+        <PerformCard tagFontSize='8px' titleFontSize='12px' locationFontSize='8px' 
+        img={PerformThumbnail} date={"D-8"} payment={"유료"} title={"스너글 페스티벌 2023"} location={"잠실종합운동장"} scraped={false}/>
+      </div>
     </div>
   )
 }
