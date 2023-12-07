f8a7a31fd2f9d67cac9b4495b878d17ed83783d9
jessy <yaeunjess@gmail.com>
2023-12-07 19:49:10 +0900

    [FIX] Delete Axios in /perform/detail

--- src/pages/PerformDetail.jsx
+++ src/pages/PerformDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import thumbnail from "../assets/performDetail-thumbnail.jpg";
 import scrapIcon from "../assets/all-icon-scrap-gray.svg";
